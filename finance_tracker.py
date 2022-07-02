5cab5db30bca8a542eaae7ca4da5e6dc7cf80284
MutiCodes <m.etterphoya@gmail.com>
2022-07-02 04:38:36 +0200

    created a dataframe that accepts source of CHF expenditure

--- finance_tracker.py
+++ finance_tracker.py
@@ -1,5 +1,6 @@
 #Pandas dataframe to define columns and hold figures
 import pandas as pd
+pd.DataFrame({'CHF-Cash':[], 'CHF-Tranfer':[]})
 pass
 #Input an expense value
 input("Please enter figure: ")
